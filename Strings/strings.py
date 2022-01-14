fce021548f293eed319aed7fa6739b4c4763d34f
mathew <97100930+mathew0ashraf@users.noreply.github.com>
2022-01-14 02:44:31 +0200

    Update and rename strings2.py to strings.py

--- Strings/strings.py
+++ Strings/strings.py
@@ -1,3 +1,49 @@
+# -------------
+# -- Strings --
+# -------------
+
+myStringOne = 'This is Single Quote'
+myStringTwo = "This is Double Quotes"
+
+print(myStringOne)
+print(myStringTwo)
+
+myStringThree = 'This is Single Quote "Test"'
+myStringFour = "This is Double Quotes 'Test'"
+
+print(myStringThree)
+print(myStringFour)
+
+myStringFive = '''First
+Second 'Test' "Test"
+Third'''
+
+myStringSix = """First
+Second "Test" \\\ 'Test'
+Third"""
+
+print(myStringFive)
+print(myStringSix)
+
+
+#OUTPUT
+
+'''
+This is Single Quote
+This is Double Quotes
+This is Single Quote "Test"
+This is Double Quotes 'Test'
+First
+Second 'Test' "Test"
+Third
+First
+Second "Test" \\ 'Test'
+Third
+'''
+
+"..................................."
+
+
 # strip() rstrip() lstrip()
 
 a = "    I Love Python    "
